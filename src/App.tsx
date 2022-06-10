90ab4f42e407945d189bdb1bd2a2631feb61c443
unset <unset>
2022-06-11 01:08:02 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const logo: string = require('./logo.svg').default;
+
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
