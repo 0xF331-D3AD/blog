c252926b76e561dd26fedae77d1bda69bb4314dd
unset <unset>
2022-06-13 05:54:30 +0300

    Fixed creation of resource url

--- src/Utils/MarkdownUtils.ts
+++ src/Utils/MarkdownUtils.ts
@@ -2,8 +2,14 @@ import {Environment} from "../Enums/Environment";
 
 export const buildFileUrlFromPathname = (pathname: string): URL => {
     const pathnameNoBaseLocation = pathname.replace(Environment.BASE_LOCATION, '');
-    if (!pathnameNoBaseLocation.endsWith('.md')){
+    if (pathnameNoBaseLocation === ''){
         const url = `${Environment.CONTENT_BASE_URL}${Environment.CONTENT_DIRECTORY_INDEX_FILENAME}`;
+        return new URL(url);
+    } else if (!pathnameNoBaseLocation.endsWith('.md')) {
+        const pathnameNoBaseLocationNoTrailingSlash = pathnameNoBaseLocation.endsWith('/')
+            ? pathnameNoBaseLocation.slice(0, pathnameNoBaseLocation.length - 1)
+            : pathnameNoBaseLocation;
+        const url = `${Environment.CONTENT_BASE_URL}${pathnameNoBaseLocationNoTrailingSlash}/${Environment.CONTENT_DIRECTORY_INDEX_FILENAME}`;
         return new URL(url);
     }
     const url = `${Environment.CONTENT_BASE_URL}${pathnameNoBaseLocation}`;
