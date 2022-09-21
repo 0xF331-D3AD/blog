dafe9a39e29fba46fc0501c0811cd1a0824be8e0
unknown <unknown>
2022-09-22 02:38:30 +0300

    Dirty environment fix

--- src/Enums/Environment.ts
+++ src/Enums/Environment.ts
@@ -1,4 +1,4 @@
 export const Environment = Object.freeze({
-    CONTENT_BASE_URL: process.env.REACT_APP_CONTENT_BASE_URL!,
-    CONTENT_DIRECTORY_INDEX_FILENAME: process.env.REACT_APP_CONTENT_DIRECTORY_INDEX_FILENAME!,
+    CONTENT_BASE_URL: process.env.REACT_APP_CONTENT_BASE_URL! || 'https://raw.githubusercontent.com/0xF331-D3AD/blog/master/Content/',
+    CONTENT_DIRECTORY_INDEX_FILENAME: process.env.REACT_APP_CONTENT_DIRECTORY_INDEX_FILENAME! || 'index.md',
 });
